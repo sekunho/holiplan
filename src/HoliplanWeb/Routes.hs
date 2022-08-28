04bbf5fa8909b3324b3c6c2cc1793967bb0fa50f
SEKUN <sekun@hey.com>
2022-08-29 06:42:15 +0800

    wip: Full on STM for funsies

--- src/HoliplanWeb/Routes.hs
+++ src/HoliplanWeb/Routes.hs
@@ -29,12 +29,13 @@ import Servant (
   Handler,
   Proxy (Proxy),
   Server,
-  type (:<|>) ((:<|>)),
+  type (:<|>) ((:<|>)), ServerError (errBody), throwError, err404
  )
 
 import Control.Concurrent.STM (TVar)
-import qualified Control.Concurrent.STM as STM (atomically, writeTVar, readTVar, readTVarIO)
+import qualified Control.Concurrent.STM as STM (atomically, readTVar, readTVarIO, writeTVar)
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.Foldable as Foldable (find)
 import Servant.API (
   Capture,
   DeleteNoContent,
@@ -55,17 +56,17 @@ import Servant.API (
 import Servant.Server (serve)
 
 type HoliplanAPI =
-  "plans" :> Capture "plan_id" Int :> Get '[JSON] PlanDetails
-    :<|> "plans" :> Get '[JSON] [PlanDetails]
-    :<|> "plans" :> ReqBody '[JSON] ReqPlan :> PostCreated '[JSON] Plan
+  "plans" :> Capture "plan_id" Int :> Get '[JSON] PlanDetail
+    :<|> "plans" :> Get '[JSON] [PlanDetail]
+    :<|> "plans" :> ReqBody '[JSON] ReqPlan :> PostCreated '[JSON] PlanDetail
 
-data PlanDetails = PlanDetails
-  { plan_details_id :: Int
-  , plan_details_date :: Day
-  , plan_details_name :: Text
-  , plan_details_description :: Text
-  , plan_details_events :: [Text]
-  , plan_details_comments :: [Text]
+data PlanDetail = PlanDetail
+  { plan_detail_id :: Int
+  , plan_detail_date :: Day
+  , plan_detail_name :: Text
+  , plan_detail_description :: Text
+  , plan_detail_events :: [Text]
+  , plan_detail_comments :: [Text]
   }
   deriving stock (Eq, Show, Generic)
 
@@ -84,57 +85,52 @@ data ReqPlan = ReqPlan
   }
   deriving stock (Eq, Show, Generic)
 
-$(deriveJSON defaultOptions{fieldLabelModifier = drop 13} ''PlanDetails)
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 13} ''PlanDetail)
 $(deriveJSON defaultOptions{fieldLabelModifier = drop 5} ''Plan)
 $(deriveJSON defaultOptions{fieldLabelModifier = drop 9} ''ReqPlan)
 
-planDetails =
-  PlanDetails
-    { plan_details_id = 1
-    , plan_details_date =
-        fromJust $
-          parseTimeM True defaultTimeLocale "%Y-%m-%d" "2022-08-27"
-    , plan_details_name = "Bob's Birthday Bash"
-    , plan_details_description = "It's Bob's birthday!"
-    , plan_details_events = []
-    , plan_details_comments = []
-    }
-
-plan =
-  Plan
-    { plan_id = 1
-    , plan_date =
-        fromJust $
-          parseTimeM True defaultTimeLocale "%Y-%m-%d" "2022-08-27"
-    , plan_name = "Bob's Birthday Bash"
-    , plan_description = "It's Bob's birthday!"
-    }
-
-server :: TVar [PlanDetails] -> Server HoliplanAPI
+server :: TVar [PlanDetail] -> Server HoliplanAPI
 server tvar = do
-  getPlan
+  getPlan tvar
     :<|> listPlans tvar
     :<|> createPlan tvar
  where
-  getPlan :: Int -> Handler PlanDetails
-  getPlan _ = do
-    liftIO $ putStrLn "hey"
+  getPlan :: TVar [PlanDetail] -> Int -> Handler PlanDetail
+  getPlan tvar planId = do
+    planDetails <- liftIO $ STM.readTVarIO tvar
 
-    pure planDetails
+    let planDetail = Foldable.find ((== planId) . plan_detail_id) planDetails
 
-  listPlans :: TVar [PlanDetails] -> Handler [PlanDetails]
+    case planDetail of
+      Just p -> pure p
+      Nothing -> throwError (err404 { errBody = "Plan doesn't exist" })
+
+  listPlans :: TVar [PlanDetail] -> Handler [PlanDetail]
   listPlans tvar = do
     liftIO $ STM.readTVarIO tvar
 
-  createPlan :: TVar [PlanDetails] -> ReqPlan -> Handler Plan
+  createPlan :: TVar [PlanDetail] -> ReqPlan -> Handler PlanDetail
   createPlan tvar reqPlan = do
     liftIO . STM.atomically $ do
       currentDetails <- STM.readTVar tvar
-      STM.writeTVar tvar (planDetails:currentDetails)
 
-    liftIO $ print reqPlan
+      let newId = case currentDetails of
+            [] -> 1
+            c : _ -> plan_detail_id c + 1
 
-    pure plan
+      let newPlanDetail =
+            PlanDetail
+              { plan_detail_id = newId
+              , plan_detail_name = req_plan_name reqPlan
+              , plan_detail_description = req_plan_description reqPlan
+              , plan_detail_date = req_plan_date reqPlan
+              , plan_detail_events = []
+              , plan_detail_comments = []
+              }
+
+      STM.writeTVar tvar (currentDetails ++ [newPlanDetail])
+
+      pure newPlanDetail
 
 holiplanAPI :: Proxy HoliplanAPI
 holiplanAPI = Proxy
