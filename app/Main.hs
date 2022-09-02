6c0d8e1226d3b04a1543b9065c0b90c9aaea51fd
SEKUN <sekun@hey.com>
2022-09-02 19:36:01 +0800

    refactor: Remove STM

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,8 @@
 module Main (main) where
 
-import qualified Control.Concurrent.STM as STM (newTVarIO)
 import qualified DB (pool)
 import HoliplanWeb.Server (app)
 import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
-main = do
-  planDetails <- STM.newTVarIO []
-  dbPool <- DB.pool 20
-
-  run 8080 (app dbPool)
+main = DB.pool 20 >>= \dbPool -> run 8080 (app dbPool)
