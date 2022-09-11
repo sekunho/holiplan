2b736fafd05983469a0f167d0fa096577660b4be
SEKUN <sekun@hey.com>
2022-09-11 15:02:18 +0800

    chore: Run on different port for now

--- app/Main.hs
+++ app/Main.hs
@@ -5,4 +5,4 @@ import HoliplanWeb.Server (app)
 import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
-main = DB.pool 20 >>= \dbPool -> run 8080 (app dbPool)
+main = DB.pool 20 >>= \dbPool -> run 8082 (app dbPool)
